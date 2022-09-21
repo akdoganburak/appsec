d22b319227054670e23025554873f44b8aaa89a3
intrapus <60838036+intrapus@users.noreply.github.com>
2022-09-21 15:04:26 +0300

    Create read.sh

---
+++ maven/read.sh
@@ -0,0 +1,9 @@
+#realease
+https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.5.0/
+
+#default repo folder
+C:\Users\Administrator\.m2\repository
+
+#fortify plugin src
+C:\Program Files\Fortify\Fortify_SCA_and_Apps_22.1.1\plugins\maven\maven-plugin-src
+mvn clean install
