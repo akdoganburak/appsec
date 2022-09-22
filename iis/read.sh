04cf9730b3f72b5f1e8ab98fe310681866e3644e
intrapus <60838036+intrapus@users.noreply.github.com>
2022-09-22 19:04:44 +0300

    Update read.sh

--- iis/read.sh
+++ iis/read.sh
@@ -3,3 +3,7 @@ C:\Windows/System32/inetsvr/config/applicationHost.config
 
 #detailed errors
 error pages > edit feature settings > detailed errors
+
+#allow unlisted extentions
+<requestFiltering> 
+                <fileExtensions allowUnlisted="true" applyToWebDAV="true">
