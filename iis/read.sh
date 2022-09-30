03acae739d58504722c85da786cd2229234fd229
intrapus <60838036+intrapus@users.noreply.github.com>
2022-09-30 11:00:04 +0300

    Update read.sh

--- iis/read.sh
+++ iis/read.sh
@@ -7,3 +7,8 @@ error pages > edit feature settings > detailed errors
 #allow unlisted extentions
 <requestFiltering> 
                 <fileExtensions allowUnlisted="true" applyToWebDAV="true">
+
+
+Open web.config
+Navigate to the node //configuration/netsparker/settings/
+Insert or replace the attribute allowAnonymousWizard with value true
