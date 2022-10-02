e6f341e72b2ce3f5ac1f4bf0c9df2348679ad527
intrapus <60838036+intrapus@users.noreply.github.com>
2022-10-02 12:03:09 +0300

    Update fortifyHC.ps1

--- fortify/fortifyHC.ps1
+++ fortify/fortifyHC.ps1
@@ -1,4 +1,4 @@
-# powershell "IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/intrapus/appsec/master/fortify/fortifyScaHC.ps1')" 
+# powershell "IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/intrapus/appsec/master/fortify/fortifyHC.ps1')" 
 
 param (
     [String[]]$MODE,
